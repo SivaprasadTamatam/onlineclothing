18a8dc0659eb08b50af1680d11abcc7231e378d3
Sivaprasad Tamatam <Sivaprasad.Tamatam@wdc.com>
2020-02-24 23:23:30 +0530

    integrated firebase into application, created sign in pagewith sign in with google

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import SHOP_DATA from "./shop.data.js";
+import React from 'react';
+
+import SHOP_DATA from './shop.data.js';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview';
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       collections: SHOP_DATA
     };
@@ -14,7 +16,7 @@ class ShopPage extends React.Component {
   render() {
     const { collections } = this.state;
     return (
-      <div className="shop-page">
+      <div className='shop-page'>
         {collections.map(({ id, ...otherCollectionProps }) => (
           <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
